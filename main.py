ddb889c44a3ea01f24d4f98cd04093096423dd26
Enes Berke İnal <enesberke2@gmail.com>
2021-01-03 20:58:05 +0300

    Ödevlerle ilgili açıklamalar.

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-print("zasdasdas")
-print("SDD")
-print("sd")
-print("Commit")
+#Its just all my homework
+
+"""
+ ---------------   4. Hw is: --------------
+Telefon rehberi uygulaması..
+
+Telefon numaralarını ve Ad Soyad bilgilerinin kullanıcı tarafından girilmesi ve onların dosyaya yazılması şeklinde bir program yazınız.
+Devam etmek veya çıkmak istiyor musun diye soran(E/H) ve  çıkış seçilirse de Tüm rehberi listeleyen bir program  yazınız..
+
+-----------------  5. Hw is: ----------------
+Ödev 5 - Fonksiyon
+Kullanıcının girdiği veriye göre;
+• Yazı ise harflerine ayırıp ekrana yazan fonksiyonu,
+• Sayı ise Asal olup olmadığını kontrol eden fonksiyonu,
+• Tekrar yazmak isteyip istemediğini sorup çıkma işlemini gerçekleştiren fonksiyonu içeren
+(toplamda en az 3 fonskiyon barından) bir program yazınız.
+
+-----------------  6. Hw is: ------------------
+Hata yakalama..
+• 1 ile 100 arası bir sayıyı kodda yazıp (Örnek; sayi=50 gibi),
+• Kullanıcının sayı girmesi istenecek ve girdiği sayı ile bizim tanımladığımız sayıyı karşılaştırıp doğru ise tebrik edecek,
+• Yanlış ise bulamadın yazacak,
+• Bunu 3 kez tekrarlayacak,
+• Kullanıcı harf girerse Lütfen sayı giriniz yazacak,
+• Kullanıcı eksi sayı girerse hata oluştu mesajı ile birlitkte ayrıntılarını verecek,
+• Veri girişi, kontrol ve 3 kez olması kısımlarını da fonksiyon ile tanımlancak olan bir
+• Programı try-except ile birlikte yazınız.
+
+---------------------  7. Hw is:  --------------------
+Listeler kodlar mevcut.
+
+
+
+
+
+
+"""
